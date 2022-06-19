3623eb3d4eebac160634ca84c868e7b15203ed73
Daniil Shulenin <d-shulenin@yandex.ru>
2022-06-19 13:42:53 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const burger = document.querySelector('.header__burger')
+const menu = document.querySelector('.header__menu')
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e){
         e.preventDefault()
@@ -6,3 +9,38 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         })
     })
 })
+
+burger.addEventListener('click', function() {
+    menu.classList.toggle('active')
+    }
+)
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function toggleActive() {
+    if(burger.classList.contains('opened')) {
+        menu.classList.remove('active')
+        burger.classList.toggle('opened')
+    }
+}
+
+function onVisible(element, callback) {
+    new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if(!entry.intersectionRatio > 0) {
+          callback();
+        }
+      });
+    }).observe(element);
+  }
+
+onVisible(menu, toggleActive)
+
